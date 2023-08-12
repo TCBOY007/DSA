5133c158fb8e6968c48a58d7d51fe6949f935faa
TCBOY007 <100079847+TCBOY007@users.noreply.github.com>
2023-08-12 20:56:39 +0800

    Add files via upload

---
+++ entity/Course.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import adt.LinkedList;
+
+/**
+ *
+ * @author chenc
+ */
+public class Course {
+
+    private String courseCode;
+    private String courseName;
+    private String courseDecription;
+    private String courseCreditHour;
+    private String courseClass; //practical ? Lecture ? Tutorial ?
+    private LinkedList<Programme> Programme;
+
+}
