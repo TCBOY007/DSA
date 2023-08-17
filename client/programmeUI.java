8d0005fc1abda5a68769a100d0f077ce55592042
z0z0z0z0z0 <131263912+z0z0z0z0z0@users.noreply.github.com>
2023-08-18 00:46:26 +0800

    Update programmeUI.java

--- client/programmeUI.java
+++ client/programmeUI.java
@@ -30,14 +30,15 @@ public class programmeUI {
                 System.out.println("|              PROGRAMMES                |");
                 System.out.println("==========================================");
                 System.out.println("[1] Programmes Details");
-                System.out.println("[2] Courses of Programmes Details");
-                System.out.println("[3] Intrsuctors of Programmes Details");
-                System.out.println("[4] Exit");
+                System.out.println("[2] Tutorial Groups of Programmes");
+                System.out.println("[3] Courses of Programmes Details");
+                System.out.println("[4] Intrsuctors of Programmes Details");
+                System.out.println("[5] Exit");
                 System.out.println("-----------------------------------------");
                 System.out.print("Choice: ");
                 choice = scanner.nextInt();
                 
-                if(choice < 1 || choice > 4){
+                if(choice < 1 || choice > 5){
                     System.out.println("Invalid Option! Please try again");
                     betterUI.pauseFor2Second();
                     betterUI.systemCls();
@@ -57,8 +58,8 @@ public class programmeUI {
                     
                 }
                 
-            }while(choice < 1 || choice > 4);
-        }while(choice != 4);
+            }while(choice < 1 || choice > 5);
+        }while(choice != 5);
         
 
     }
@@ -107,7 +108,7 @@ public class programmeUI {
                         listProgramme();
                         break;
                     case 5:
-                        
+                        removeProgramme();
                         break;
 
                 }
@@ -121,42 +122,86 @@ public class programmeUI {
         Programme newProg = new Programme();
         boolean chk = true;
         boolean chk2 = true;
+        boolean chk3 = true;
         
         System.out.print("Enter programme name :");
         newProg.setProgrammeName(scanner.nextLine());
         System.out.print("Enter programme code :");
         newProg.setProgrammeCode(scanner.nextLine().toUpperCase());
-        System.out.print("Enter programme type :");
-        newProg.setProgrammeType(scanner.nextLine().toUpperCase());
         
         do {
-            System.out.print("Enter programme duration(Years) :");
+            
+            System.out.println("Enter programme type :");
+            System.out.println("[1] Diploma [2] Degreee [3] Master");
+            System.out.println("--------------------------------------");
+            System.out.print("Choice: ");
             if (scanner.hasNextInt()) {
-                newProg.setProgrammeDuration(scanner.nextInt());
-                chk = false;
-                break;
+                int type = scanner.nextInt();  
+                
+                if(type <= 1 || type <= 3){
+                    String typeInput = "";
+                    switch (type) {
+                        case 1:
+                            typeInput = "DIPLOMA";
+                            break;
+                        case 2:
+                            typeInput = "DEGREE";
+                            break;
+                        case 3:
+                            typeInput = "MASTER";
+                            break;
+                    }
+                    newProg.setProgrammeType(typeInput);
+
+                    chk = false;
+                    break;
+                }
+                else{
+                    System.out.println("Invalid Option! Please try again\n");
+                }
+                
             } else {
                 scanner.nextLine();
+                System.out.println("Invalid input! Please enter a valid numerical value.\n");
+            }
+            
+        } while (chk);
+        
+        
+        do {
+            scanner.nextLine();
+            System.out.print("Enter programme duration(years) :");
+            if (scanner.hasNextInt()) {
+                newProg.setProgrammeDuration(scanner.nextInt());
+                chk2 = false;
+                break;
+            } else {
                 System.out.println("Invalid input! Please enter a valid numerical value.");
             }
 
-        } while (chk);
+        } while (chk2);
         
         do{
+            scanner.nextLine();
             System.out.print("Enter programme fee :");
             if(scanner.hasNextDouble()){
                 newProg.setProgrammeFee(scanner.nextDouble());
-                chk2 = false;
+                chk3 = false;
                 break;
             }else{
-                scanner.nextLine();
                 System.out.println("Invalid input! Please enter a valid numerical value.");
             }
             
-        }while(chk2);
+        }while(chk3);
         
         
-        System.out.println(progManage.addProgamme(newProg));
+        if(progManage.addProgamme(newProg)){
+            System.out.println("New Programmed Added Successfully !");
+            betterUI.pauseFor2Second();
+        }
+        else{
+            System.out.println("No Programme Added !");
+        }
         
     }
     
@@ -192,17 +237,59 @@ public class programmeUI {
                     case 1:
                         System.out.print("Search by Name: ");
                         String nameInput = scanner.nextLine();
-                        progManage.searchProgramme(nameInput.toUpperCase(), "name");
+                        ListInterface<Programme> listByName = progManage.searchProgramme(nameInput.toUpperCase(), "name");
+                        
+                        System.out.println("Programme Found: ");
+                        for(int i=1; i<=listByName.getNumberOfEntries(); i++){
+                            System.out.println(listByName.getEntry(i));
+                        }
+                        
+                        if(listByName.isEmpty())
+                            System.out.println("No Programme !");
+                        
                         break;
                     case 2:
                         System.out.print("Search by Code: ");
                         String codeInput = scanner.nextLine();
-                        progManage.searchProgramme(codeInput.toUpperCase(), "code");
+                        ListInterface<Programme> listByCode = progManage.searchProgramme(codeInput.toUpperCase(), "code");
+                        
+                        System.out.println("Programme Found: ");
+                        for (int i = 1; i <= listByCode.getNumberOfEntries(); i++) {
+                            System.out.println(listByCode.getEntry(i));
+                        }
+                        
+                        if (listByCode.isEmpty())
+                            System.out.println("No Programme !");
                         break;
                     case 3:
                         System.out.print("Search by Type: ");
-                        String typeInput = scanner.nextLine();
-                        progManage.searchProgramme(typeInput.toUpperCase(), "code");
+                        System.out.println("[1] Diploma [2] Degreee [3] Master");
+                        System.out.println("--------------------------------------");
+                        System.out.print("Choice: ");                        
+                        int type = scanner.nextInt();
+                        
+                        String typeInput ="";
+                        switch(type){
+                            case 1:
+                                typeInput = "DIPLOMA";
+                                break;
+                            case 2:
+                                typeInput = "DEGREE";
+                                break;
+                            case 3:
+                                typeInput = "MASTER";
+                                break;
+                        }
+                        
+                        ListInterface<Programme> listByType = progManage.searchProgramme(typeInput, "type");
+                        
+                        System.out.println("Programme Found: ");
+                        for (int i = 1; i <= listByType.getNumberOfEntries(); i++) {
+                            System.out.println(listByType.getEntry(i));
+                        }
+                        
+                        if (listByType.isEmpty())
+                            System.out.println("No Programme !");
                         break;
 
                 }
@@ -213,6 +300,29 @@ public class programmeUI {
     
     public static void listProgramme(){
         progManage.printAll();
+    }
+    
+    public static void removeProgramme(){
+        System.out.println("Select the type of programme:");
+        System.out.println("[1] Diploma [2] Degreee [3] Master");
+        System.out.println("--------------------------------------");
+        System.out.print("Choice: ");
+        int choice = scanner.nextInt();
+        
+        switch(choice){
+            case 1:
+                ListInterface<Programme> diplomaList = progManage.searchProgramme("DIPLOMA", "type");
+                
+                break;
+            case 2:
+                ListInterface<Programme> degreeList = progManage.searchProgramme("DEGREE", "type");
+                
+                break;
+            case 3:
+                ListInterface<Programme> masterList = progManage.searchProgramme("MASTER", "type");
+                
+                break;
+        }
     }
     
     public static void coursesSubMenu() {
