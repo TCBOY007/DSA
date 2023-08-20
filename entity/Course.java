9c4c597c6dc4ad4df25a6f7539ad909d77fb0acc
TCBOY007 <100079847+TCBOY007@users.noreply.github.com>
2023-08-20 14:40:47 +0800

    Update Course.java

--- entity/Course.java
+++ entity/Course.java
@@ -14,9 +14,56 @@ public class Course {
 
     private String courseCode;
     private String courseName;
-    private String courseDecription;
-    private String courseCreditHour;
-    private String courseClass; //practical ? Lecture ? Tutorial ?
+    private String courseDescription;
+    private int courseCreditHour;
     private LinkedList<Programme> Programme;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public int getCourseCreditHour() {
+        return courseCreditHour;
+    }
+
+    public void setCourseCreditHour(int courseCreditHour) {
+        this.courseCreditHour = courseCreditHour;
+    }
+
+    public LinkedList<Programme> getProgramme() {
+        return Programme;
+    }
+
+    public void setProgramme(LinkedList<Programme> Programme) {
+        this.Programme = Programme;
+    }
+    
+    
+
+   
+    
+    
+    
+    
 
 }
