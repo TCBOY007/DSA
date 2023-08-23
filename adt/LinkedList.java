a415acb9505cbf1014434326afda9cb25fafae71
z0z0z0z0z0 <131263912+z0z0z0z0z0@users.noreply.github.com>
2023-08-23 17:58:27 +0800

    Update LinkedList.java

--- adt/LinkedList.java
+++ adt/LinkedList.java
@@ -13,146 +13,113 @@ import java.util.function.Predicate;
  */
 public class LinkedList<T> implements ListInterface<T> {
 
-    private Node firstNode; // reference to first node
-    private int numberOfEntries;  	// number of entries in list
+    private Node firstNode;
+    private int totalNumberData;
 
     public LinkedList() {
-        clear();
+        initialize();
     }
 
     @Override
-    public final void clear() {
+    public final void initialize() {
         firstNode = null;
-        numberOfEntries = 0;
+        totalNumberData = 0;
     }
 
     @Override
-    public boolean add(T newEntry) {
-        Node newNode = new Node(newEntry);	// create the new node
+    public boolean add(T newData) {
+        Node newNode = new Node(newData);
 
-        if (isEmpty()) {
+        if (!isEmpty()) {
+            Node lastNode = getLastNode();
+            lastNode.next = newNode;
+        } else {                      
             firstNode = newNode;
-        } else {                        // add to end of nonempty list
-            Node currentNode = firstNode;	// traverse linked list with p pointing to the current node
-            while (currentNode.next != null) { // while have not reached the last node
-                currentNode = currentNode.next;
-            }
-            currentNode.next = newNode; // make last node reference new node
         }
 
-        numberOfEntries++;
+        totalNumberData++;
         return true;
     }
 
     @Override   
-    public boolean add(int newPosition, T newEntry) { // OutOfMemoryError possible
-        boolean isSuccessful = true;
+    public boolean add(int newPosition, T newData) { 
+        if (newPosition < 1 || newPosition > totalNumberData + 1) {
+            return false; // Invalid position
+        }
 
-        if ((newPosition >= 1) && (newPosition <= numberOfEntries + 1)) {
-            Node newNode = new Node(newEntry);
+        Node newNode = new Node(newData);
 
-            if (isEmpty() || (newPosition == 1)) { // case 1: add to beginning of list
-                newNode.next = firstNode;
-                firstNode = newNode;
-            } else {								// case 2: list is not empty and newPosition > 1
-                Node nodeBefore = firstNode;
-                for (int i = 1; i < newPosition - 1; ++i) {
-                    nodeBefore = nodeBefore.next;		// advance nodeBefore to its next node
-                }
-
-                newNode.next = nodeBefore.next;	// make new node point to current node at newPosition
-                nodeBefore.next = newNode;		// make the node before point to the new node
-            }
-
-            numberOfEntries++;
+        if (isEmpty() || newPosition == 1) {
+            newNode.next = firstNode;
+            firstNode = newNode;
         } else {
-            isSuccessful = false;
+            Node previousNode = getNodeAt(newPosition - 1);
+            newNode.next = previousNode.next;
+            previousNode.next = newNode;
         }
 
-        return isSuccessful;
+        totalNumberData++;
+        return true;
     }
 
     @Override
-    public boolean remove(int givenPosition) {
-        int tempNumberEntries = numberOfEntries;
-        if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
-            if (givenPosition == 1) {      // case 1: remove first entry
-                firstNode = firstNode.next;
-            } else {                         // case 2: givenPosition > 1
-                Node nodeBefore = firstNode;
-                for (int i = 1; i < givenPosition -1; ++i) {
-                    nodeBefore = nodeBefore.next;		// advance nodeBefore to its next node
-                }
-                
-                nodeBefore.next = nodeBefore.next.next;	// make node before point to node after the
-            } 																// one to be deleted (to disconnect node from chain)
-
-            numberOfEntries--;
-            
+    public boolean remove(int position) {
+        if (position < 1 || position > totalNumberData) {
+            return false; // Invalid position
         }
 
-        return (tempNumberEntries - 1) == numberOfEntries; // return removed entry, or null if operation fails
-    }
-
-    @Override
-    public boolean replace(int givenPosition, T newEntry) {
-        boolean isSuccessful = true;
-
-        if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
-            Node currentNode = firstNode;
-            for (int i = 0; i < givenPosition - 1; ++i) {
-                currentNode = currentNode.next;		// advance currentNode to next node
-            }
-            currentNode.data = newEntry;	// currentNode is pointing to the node at givenPosition
+        if (position == 1) { // Remove the first node
+            firstNode = firstNode.next;
         } else {
-            isSuccessful = false;
+            Node nodeBefore = getNodeAt(position - 1);
+            nodeBefore.next = nodeBefore.next.next;
         }
 
-        return isSuccessful;
+        totalNumberData--;
+        return true;
     }
 
     @Override
-    public T getEntry(int givenPosition) {
-        T result = null;
+    public boolean replace(int position, T newData) {
+        if (position < 1 || position > totalNumberData) {
+            return false; // Invalid position
+        }
 
-        if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
-            Node currentNode = firstNode;
-            for (int i = 0; i < givenPosition - 1; ++i) {
-                currentNode = currentNode.next;		// advance currentNode to next node
+        Node nodeToReplace = getNodeAt(position);
+        nodeToReplace.data = newData;
+
+        return true;
+    }
+
+    @Override
+    public T getData(int position) {
+        if (position < 1 || position > totalNumberData) {
+            return null; // Invalid position
+        }
+
+        Node nodeToGet = getNodeAt(position);
+        return nodeToGet.data;
+    }
+
+    @Override
+    public boolean contains(T data) {
+
+        for (int i = 1; i <= totalNumberData; i++) {
+            if (data.equals(getData(i))) {
+                return true;
             }
-            result = currentNode.data;	// currentNode is pointing to the node at givenPosition
         }
-
-        return result;
+        return false;
     }
 
     @Override
-    public boolean contains(T anEntry) {
-        boolean found = false;
-        Node currentNode = firstNode;
-
-        while (!found && (currentNode != null)) {
-            if (anEntry.equals(currentNode.data)) {
-                found = true;
-            } else {
-                currentNode = currentNode.next;
-            }
-        }
-        return found;
-    }
-
-    @Override
-    public int getNumberOfEntries() {
-        return numberOfEntries;
+    public int getTotalNumberData() {
+        return totalNumberData;
     }
 
     @Override
     public boolean isEmpty() {
-        boolean result;
-
-        result = numberOfEntries == 0;
-
-        return result;
+        return (totalNumberData == 0);
     }
 
     @Override
@@ -162,13 +129,14 @@ public class LinkedList<T> implements ListInterface<T> {
 
     @Override
     public String toString() {
-        String outputStr = "";
+        String output = "";
         Node currentNode = firstNode;
-        while (currentNode != null) {
-            outputStr += currentNode.data + "\n";
+
+        for (int i = 1; i <= totalNumberData; i++) {
+            output += currentNode.data + "\n";
             currentNode = currentNode.next;
         }
-        return outputStr;
+        return output;
     }
 
     @Override
@@ -180,7 +148,7 @@ public class LinkedList<T> implements ListInterface<T> {
             }
             currentNode = currentNode.next;
         }
-        return null; // Return null if no matching entry is found    
+        return null; // Return null if no matching data is found    
     }
 
     
@@ -198,6 +166,24 @@ public class LinkedList<T> implements ListInterface<T> {
             this.data = data;
             this.next = next;
         }
+    }
+    
+    private Node getLastNode() {
+        
+        Node currentNode = firstNode;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        
+        return currentNode;
+    }
+    
+    private Node getNodeAt(int position) {
+        Node currentNode = firstNode;
+        for (int i = 1; i < position; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
     }
 
 }
