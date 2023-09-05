6be6d780410cf7ba1ecf352a93f2e954a3824c1e
z0z0z0z0z0 <131263912+z0z0z0z0z0@users.noreply.github.com>
2023-09-05 19:46:31 +0800

    Add files via upload

---
+++ adt/LinkedStackInterface.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package adt;
+
+/**
+ *
+ * @author chook
+ */
+public interface LinkedStackInterface<T> {
+
+    public void push(T newData);
+
+    public T pop();
+
+    public T peek();
+
+    public int getTotalNumberData();
+    
+    public boolean isEmpty();
+
+    public void reset();
+}
